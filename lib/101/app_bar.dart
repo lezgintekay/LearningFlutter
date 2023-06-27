1186b53aedae17166324fe5a2b84daae38f37b1f
Lezgin Tekay <tekay.lezgin@yandex.com>
2023-06-27 16:04:58 +0300

    text, appbar, container and button studies

---
+++ lib/101/app_bar.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class AppBarView extends StatelessWidget {
+  const AppBarView({super.key});
+  final String tittle = "Welcome";
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+          title: Text(
+            tittle,
+            style: const TextStyle(color: Colors.red),
+          ),
+          automaticallyImplyLeading: false,
+          leading: IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.arrow_back),
+          ),
+          actions: [
+            IconButton(onPressed: () {}, icon: const Icon(Icons.person))
+          ]),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_application_2/101/app_bar.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "Flutter Demo",
+      theme: ThemeData.dark().copyWith(
+          appBarTheme: const AppBarTheme(
+        centerTitle: true,
+        systemOverlayStyle: SystemUiOverlayStyle.light,
+        backgroundColor: Colors.red,
+        elevation: 0,
+      )),
+      home: const AppBarView(),
+    );
+  }
+}
