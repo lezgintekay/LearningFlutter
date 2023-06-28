cbfbb9b8a1dfdc9444cc37780e63c1e11c871987
Lezgin Tekay <tekay.lezgin@yandex.com>
2023-06-28 22:55:13 +0300

    indicator learn

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:flutter_application_2/101/custom_widget_learn.dart';
+import 'package:flutter_application_2/101/indicator_learn.dart';
 
 
 
@@ -21,7 +21,7 @@ class MyApp extends StatelessWidget {
         systemOverlayStyle: SystemUiOverlayStyle.light,
         elevation: 0,
       )),
-      home: const CustomWidgetLearn(),
+      home: IndicatorLearn(),
       
     );
   }
