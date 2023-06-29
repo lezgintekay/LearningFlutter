a78ece22b05451bde023e4abb5ff481175f32e87
Lezgin Tekay <tekay.lezgin@yandex.com>
2023-06-30 01:58:20 +0300

    life cycle

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:flutter_application_2/101/page_view_learn.dart';
+import 'package:flutter_application_2/lifecycle_learn.dart';
 
 
 
@@ -22,7 +22,7 @@ class MyApp extends StatelessWidget {
         systemOverlayStyle: SystemUiOverlayStyle.light,
         elevation: 0,
       )),
-      home: const PageViewLearn(),
+      home: const StatefullLifecycleLearn(message: "Test12",),
       
     );
   }
