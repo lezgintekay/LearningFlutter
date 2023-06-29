92db80081f0be4bbfda37b04baff40c53707668d
Lezgin Tekay <tekay.lezgin@yandex.com>
2023-06-29 15:16:31 +0300

    statefull widgets learned

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:flutter_application_2/101/demos/stack_demo.dart';
+import 'package:flutter_application_2/101/statefull_learn.dart';
 
 
 
@@ -22,7 +22,7 @@ class MyApp extends StatelessWidget {
         systemOverlayStyle: SystemUiOverlayStyle.light,
         elevation: 0,
       )),
-      home: const StackDemo(),
+      home: const StatefullLearn(),
       
     );
   }
